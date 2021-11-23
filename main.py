2de6338b3f6dce5b6ec66e343e9ebb1f52ff548f
tedzards509 <tedzards509@gmail.com>
2021-11-23 18:01:55 +0100

    Working template that does not leak the token

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+with open(file="./token") as f:
+    client.run(f.readlines()[0])
