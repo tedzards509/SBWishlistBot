b1f48d03838fdc879aedc0897dab1997fc328719
tedzards509 <tedzards509@gmail.com>
2021-11-24 13:08:46 +0100

    Some commands for initial testing (and learning)

---
+++ modules/commands.py
@@ -0,0 +1,50 @@
+def command_name():
+    output = "ayy you found my template"
+    return output
+
+
+def ping(client):
+    """
+    Idrk what to say. Its my ping not urs.
+    """
+    output = f"Pong! {round(client.latency * 1000)}ms"
+    return output
+
+
+def test_raw(param):
+    """Returns your input"""
+    if not param:
+        output = "Test erfolgreich"
+    else:
+        output = param
+    return output
+
+
+def test(param):
+    """Returns your input"""
+    if not param:
+        output = "Test erfolgreich"
+    else:
+        output = param
+    return output
+
+
+def sbw_help(param):
+    """
+    Bruh I'm sacrificing my docstrings to make this not as ugly and you use it like this?!
+    What do you assume this does?!
+    """
+    availaible_commands = "Availaible commands:\n" \
+                          "`sbw help` - Displays this help message\n" \
+                          "`sbw ping` - Returns my ping\n" \
+                          "`sbw test` - really only exists for testing, ignore pls\n" \
+                          "For more details use `sbw help [command]`"
+    if not param:
+        output = availaible_commands
+    elif param[0] == "help":
+        output = sbw_help.__doc__
+    elif param[0] == "ping":
+        output = ping.__doc__
+    else:
+        output = f"Unknown command `{param[0]}`, \n{availaible_commands}"
+    return output
